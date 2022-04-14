aa7e8f25871f03fa55d7f113a841eae108c810c7
Meghal Shah <78637599+mvshah10@users.noreply.github.com>
2022-04-14 18:28:59 +0530

    Create a.py

---
+++ Practical-2/Set/a.py
@@ -0,0 +1,21 @@
+## Aim:- Write a Python program to add member(s) in a set and clear a set.
+## Student_Name:- Meghal Shah
+## Student_ID:- 20CS085
+
+# Creating a set of Prime Numbers.
+setPrimeNumbers = {2, 3, 5, 7}
+
+# Prints the existing set.
+print(setPrimeNumbers)
+
+# Adding a member to the existing set 'setPrimeNumbers'. 
+setPrimeNumbers.add(11)
+
+# Prints the updated set.
+print(setPrimeNumbers)
+
+# Clearing a set using clear() method.
+setPrimeNumbers.clear()
+
+# Prints the statement after clearing the set.
+print('Set(after clear):', setPrimeNumbers)
