0d876581f423ad0c0497e4f81215272c92421195
Meghal Shah <78637599+mvshah10@users.noreply.github.com>
2022-04-14 18:27:55 +0530

    Create a.py

---
+++ Practical-2/Tuple/a.py
@@ -0,0 +1,9 @@
+## Aim:- Write a Python program to create a tuple with different data types.
+## Student_Name:- Meghal Shah
+## Student_ID:- 20CS085
+
+# Creating a tuple with string, boolean, float and numeric data types.
+Tuple = ("Meghal", True, 93.33, 10)
+
+# Prints tuple here.
+print(Tuple)
